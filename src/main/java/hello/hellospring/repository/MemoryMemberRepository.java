53389348a0d4c88513bde85b2f34e65f390628e5
RoruRoruRoru <leon122739@gmail.com>
2023-04-11 23:02:09 +0900

    new

--- src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
-public class MemoryMemberRepository implements  MemberRepository {
+public class  MemoryMemberRepository implements  MemberRepository {
     private static Map<Long, Member> store = new HashMap<>();
     private static  long sequence = 0L;
     @Override
