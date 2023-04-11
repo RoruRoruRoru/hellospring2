b9f6146d4c2c053cf8ff385d40ab091cdc765cfd
RoruRoruRoru <leon122739@gmail.com>
2023-04-11 17:39:13 +0900

    new

---
+++ src/main/java/hello/hellospring/controller/MemberForm.java
@@ -0,0 +1,13 @@
+package hello.hellospring.controller;
+
+public class MemberForm {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
+}
